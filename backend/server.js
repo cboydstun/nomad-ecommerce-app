1b1dfa4abf8c0aaa98713d90037000655cb852de
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-08-09 00:14:12 -0500

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const createCheckoutSession = require('./api/checkout')
 require('dotenv').config({ path: './.env' })
@@ -8,7 +9,7 @@ app.use(express.json())
 app.use(cors({ origin: '*' }))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
