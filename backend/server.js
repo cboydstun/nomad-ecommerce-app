a9966129f0170fb7542bac5a7c296908a798cc65
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-08-09 00:00:21 -0500

    added heroku build syntax

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const createCheckoutSession = require('./api/checkout');
-require('dotenv').config({ path: './.env' });
+const express = require('express')
+const cors = require('cors')
+const createCheckoutSession = require('./api/checkout')
+require('dotenv').config({ path: './.env' })
 
-const app = express();
-app.use(express.json());
-app.use(cors({ origin: '*' }));
+const app = express()
+app.use(express.json())
+app.use(cors({ origin: '*' }))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-app.post('/create-checkout-session', createCheckoutSession);
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
-app.listen(process.env.PORT || 5000, () => console.log('server listening on port 5000'));
+app.post('/create-checkout-session', createCheckoutSession)
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log('server listening on port 5000')
+)
